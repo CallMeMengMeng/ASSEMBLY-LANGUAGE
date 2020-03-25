8c9dc183ef05fed41d20fc6e36411cad6eac002e
Reyes Meng <39575910+CallMeMengMeng@users.noreply.github.com>
2020-03-25 17:38:55 +0800

    Update PlusMinusZero.asm

--- PlusMinusZero.asm
+++ PlusMinusZero.asm
@@ -1,4 +1,4 @@
-; THERE ARE 12 SIGN NUMBER IN TABLE, COUNT THE NUMBER OF PLUS MINUS 0,RESPECTIVELY
+; THERE ARE 12 SIGNED NUMBER IN TABLE, COUNT THE NUMBER OF PLUS MINUS 0,RESPECTIVELY
 
 DATA	SEGMENT
 TABLE	DB	23h,78h,56h,0abh,00h,0cdh,59h,00h,23h,0efh,00h,0c0h
@@ -50,10 +50,10 @@ CODE	ENDS
 ;
 ; then I got "PLUS:4, MINUS:5, ZERO:3" which meant the 98h was negative.
 ;      
-; WTF...WAITTT...That's true. Given that the scale of signed numbers
+; WTF...WAITTT...That's true. The range of signed numbers
 ;
-; presented by 1 byte is from -128 to 127, 98h equals to -104 or 152
+; presented by 1 byte is from -128 to 127, while 98h equals to -104 or 152
 ;
-; (overflowing), consequently, 98h is negative. After replacing 98h 
+; (overflowing), consequently 98h is negative. After replacing 98h 
 ; 
 ; with 23h, the result is correct.  
